220be03cde11705c54a227560b58cd5125ab11e3
Amir Suyundukov <gto.kraken@gmail.com>
2024-10-10 13:41:30 +0300

    commit test № 3

--- src/main/java/ru/suyundukov/MyProject/BusinessUnit.java
+++ src/main/java/ru/suyundukov/MyProject/BusinessUnit.java
@@ -4,15 +4,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-public class BusinessUnit {
+public class BusinessUnit extends CounterParty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String shortName;
     private String fullName;
     private int psrn;
-
     private int inn;
     private String oktmo;
-    private String centralBank;
+    private String centralBank;//енумка
 }
--- src/main/java/ru/suyundukov/MyProject/Person.java
+++ src/main/java/ru/suyundukov/MyProject/Person.java
@@ -6,14 +6,14 @@ import jakarta.persistence.Id;
 
 import java.time.LocalDate;
 
-public class Person {
+public class Person extends CounterParty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String surName;
     private String patronymic;
-    private String centralBank;
+    private String centralBank;//енумка
     private int snils;
     private LocalDate dateOfBirth;
 }
