5a24c07f84abebd7e20293a84ce30c630fbbf84e
Amir Suyundukov <gto.kraken@gmail.com>
2024-10-30 12:00:49 +0300

    commit test № 16 Добавлен Swagger(бета)

---
+++ src/main/java/ru/suyundukov/MyProject/others/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package ru.suyundukov.MyProject.others;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()  .info(new Info()
+                .title("Legal Entity API")
+                .version("1.0")
+                .description("API для управления юридическими лицами")
+                .contact(new Contact()
+                        .name("Имя Контактного Лица")
+                        .email("contact@example.com")
+                        .url("https://example.com"))
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("http://springdoc.org")));
+
+    }
+}
