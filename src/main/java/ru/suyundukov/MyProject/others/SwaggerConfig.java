8138d7194a15c5508d302474245046a05dc647aa
Amir Suyundukov <gto.kraken@gmail.com>
2024-10-30 16:40:48 +0300

    commit test № 16 Добавлен Swagger работающий

--- src/main/java/ru/suyundukov/MyProject/others/SwaggerConfig.java
+++ src/main/java/ru/suyundukov/MyProject/others/SwaggerConfig.java
@@ -2,26 +2,16 @@ package ru.suyundukov.MyProject.others;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI customOpenAPI(){
-        return new OpenAPI()  .info(new Info()
-                .title("Legal Entity API")
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("My Project")
                 .version("1.0")
-                .description("API для управления юридическими лицами")
-                .contact(new Contact()
-                        .name("Имя Контактного Лица")
-                        .email("contact@example.com")
-                        .url("https://example.com"))
-                .license(new License()
-                        .name("Apache 2.0")
-                        .url("http://springdoc.org")));
-
+                .description("Создание UL IT IP и изменение"));
     }
 }
